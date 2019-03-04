8663fa388f0bf08ac1c056434c11d623487660d2
davr0 <45453137+davr0@users.noreply.github.com>
2019-03-04 11:57:44 +0100

    Add files via upload

---
+++ 1_mac_changer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+#import the subprocess module
+import subprocess
+import optparse
+import re
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info.")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def get_current_mac(interface):
+    # Check de result van ifconfig + de interface
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+
+    # Regex en zoek in 'infconfig_result'
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read the MAC address")
+
+#get arguments that the user enters
+options = get_arguments()
+#get the current mac
+current_mac = get_current_mac(options.interface)
+#print the current mac
+print("Current MAC = " + str(current_mac)) #casting
+#change the mac address
+change_mac(options.interface, options.new_mac)
+
+#check if the user arguments really got changed
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was succesfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed. ")
